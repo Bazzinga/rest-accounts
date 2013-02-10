9eccd72f8f304c9bab222d83a084ac6833e677b1
Jose L. Patino <jose@travelbird.nl>
2013-02-10 18:12:11 +0100

    Moved to class based views.

--- src/rest_accounts/accounts/urls.py
+++ src/rest_accounts/accounts/urls.py
@@ -2,10 +2,12 @@ from django.conf.urls import patterns, url
 
 from rest_framework.urlpatterns import format_suffix_patterns
 
+from accounts import views
 
-urlpatterns = patterns('accounts.views',
-    url(r'^accounts/$', 'account_list'),
-    url(r'^accounts/(?P<pk>[0-9]+)/$', 'account_detail'),
+
+urlpatterns = patterns('',
+    url(r'^accounts/$', views.AccountList.as_view()),
+    url(r'^accounts/(?P<pk>[0-9]+)/$', views.AccountDetail.as_view()),
 )
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- src/rest_accounts/accounts/views.py
+++ src/rest_accounts/accounts/views.py
@@ -1,48 +1,48 @@
 from django.shortcuts import get_object_or_404
 
-from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 from accounts.models import Account
 from accounts.serializers import AccountSerializer
 
 
-@api_view(['GET', 'POST'])
-def account_list(request, format=None):
+class AccountList(APIView):
     """
     List all accounts or create a new account.
     """
-    if request.method == 'GET':
+    def get(self, request, format=None):
         accounts = Account.objects.all()
         serializer = AccountSerializer(accounts)
         return Response(serializer.data)
-    elif request.method == 'POST':
+
+    def post(self, request, format=None):
         serializer = AccountSerializer(data=request.DATA)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def account_detail(request, pk, format=None):
+class AccountDetail(APIView):
     """
-    Retrieve, update or delete an existent account.
+    Retrieve, update or delete an account instance.
     """
-    account = get_object_or_404(Account, pk=pk)
-
-    if request.method == 'GET':
+    def get(self, request, pk, format=None):
+        account = get_object_or_404(Account, pk=pk)
         serializer = AccountSerializer(account)
         return Response(serializer.data)
-    elif request.method == 'PUT':
+
+    def put(self, request, pk, format=None):
+        account = get_object_or_404(Account, pk=pk)
         serializer = AccountSerializer(account, data=request.DATA)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'DELETE':
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        account = get_object_or_404(Account, pk=pk)
         account.delete()
-        return Response(status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_204_NO_CONTENT)
