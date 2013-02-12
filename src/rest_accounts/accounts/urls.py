9e6a0dcb6ed339f451f3cdfda7ff128e71b55c0c
Jose L. Patino <jose@travelbird.nl>
2013-02-12 09:57:41 +0100

    Login for browseable API.

--- src/rest_accounts/accounts/urls.py
+++ src/rest_accounts/accounts/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, url, include
 
 from rest_framework.urlpatterns import format_suffix_patterns
 
@@ -10,6 +10,8 @@ urlpatterns = patterns('',
     url(r'^accounts/(?P<pk>[0-9]+)/$', views.AccountDetail.as_view()),
     url(r'^hotels/$', views.HotelList.as_view()),
     url(r'^hotels/(?P<pk>[0-9]+)/$', views.HotelInstance.as_view()),
+    url(r'^api-auth/', include('rest_framework.urls',
+                               namespace='rest_framework')),
 )
 
 urlpatterns = format_suffix_patterns(urlpatterns)
